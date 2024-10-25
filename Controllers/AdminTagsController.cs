a0388585daea37d685b7fb274245ea718642ad2e
Craig Neff <craig.neff@driveprodigy.com>
2024-10-25 14:52:18 -0600

    24- Saving Data to Database using DbContext

--- Controllers/AdminTagsController.cs
+++ Controllers/AdminTagsController.cs
@@ -1,10 +1,20 @@
-﻿using Bloggie.Web.Models.ViewModels;
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        #region Add Tag Page
+
+        private BloggieDbContext _bloggieDbContext;
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this._bloggieDbContext = bloggieDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -14,11 +24,20 @@ namespace Bloggie.Web.Controllers
         [HttpPost]
         [ActionName("Add")]
         public IActionResult Add(AddTagRequest addTagRequest)
-        {
-            string name = addTagRequest.Name;
-            string displayName = addTagRequest.DisplayName;
+        {   
+            //Mapping AddTagRequest to Tag Domain Model
+            Tag tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            _bloggieDbContext.Tags.Add(tag);
+            _bloggieDbContext.SaveChanges();
 
             return View("Add");
         }
+
+        #endregion
     }
 }
