a1c777489ff64440fedbcb125d4605ab5abdf7d0
Craig Neff <craig.neff@driveprodigy.com>
2024-11-04 10:51:03 -0700

    33- Create new HTML Form to Add new Blog

---
+++ Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,37 @@
+﻿using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagRepository _tagRepository;
+
+        public AdminBlogPostsController(ITagRepository tagRepository)
+        {
+            this._tagRepository = tagRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            //Get stags from repository
+            var tags = await _tagRepository.GetAllAsync();
+
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() })
+            }; 
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBlogPostRequest addBlockPostRequest)
+        {
+            return RedirectToAction("Add");
+        }
+    }
+}
