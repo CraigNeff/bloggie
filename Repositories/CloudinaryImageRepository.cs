b164f4316adf4431d58b0290f2566928ca865cfb
Craig <Craig.neff@driveprodigy.com>
2024-11-14 09:42:29 -0700

    44- Register to Cloudinary, setup API information(cloudName, API-key, API-secret)

--- Repositories/CloudinaryImageRepository.cs
+++ Repositories/CloudinaryImageRepository.cs
@@ -1,11 +1,41 @@
 ﻿
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using System.Net;
+
 namespace Bloggie.Web.Repositories
 {
     public class CloudinaryImageRepository : IImageRepository
     {
-        public Task<string> UploadAsync(IFormFile file)
+        private readonly IConfiguration _configuration;
+
+        private readonly Account _account;
+        public CloudinaryImageRepository(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            this._configuration = configuration;
+            this._account = new Account(
+                    configuration.GetSection("Cloudinary")["CloudName"],
+                    configuration.GetSection("Cloudinary")["ApiKey"],
+                    configuration.GetSection("Cloudinary")["ApiSecret"]);
+        }
+        public async Task<string> UploadAsync(IFormFile file)
+        {
+            var client = new Cloudinary(_account);
+
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(file.FileName,file.OpenReadStream()),
+                DisplayName = file.FileName
+            };
+
+            var uploadResult = await client.UploadAsync(uploadParams);
+
+            if(uploadResult != null && uploadResult.StatusCode == HttpStatusCode.OK)
+            {
+                return uploadResult.SecureUri.ToString();
+            }
+
+            return null;
         }
     }
 }
