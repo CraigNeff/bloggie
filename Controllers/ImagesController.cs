2934259edc39ee226b94111543529a71924dbaae
Craig <Craig.neff@driveprodigy.com>
2024-11-15 09:03:06 -0700

    45- Inject Image Repository and Test

--- Controllers/ImagesController.cs
+++ Controllers/ImagesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace Bloggie.Web.Controllers
 {
@@ -7,11 +9,25 @@ namespace Bloggie.Web.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
+        private readonly IImageRepository _imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this._imageRepository = imageRepository;
+        }
 
         [HttpPost]
         public async Task<IActionResult> UploadAsync(IFormFile file)
         {
             //call a repository
+            var imgURL = await _imageRepository.UploadAsync(file);
+
+            if (imgURL == null)
+            {
+                return Problem("Something went wrong!", null, (int)HttpStatusCode.InternalServerError);
+            }
+
+            return new JsonResult(new { link = imgURL });
         }
     }
 }
